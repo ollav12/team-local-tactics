0f7f1517d9564cd13db0fcba6b252d396a7de204
ollav12 <olavopheim@hotmail.no>
2022-03-09 11:53:31 +0100

    got game functions from server

--- teamnetworktactics.py
+++ teamnetworktactics.py
@@ -1,6 +1,9 @@
 from rich.table import Table
-from core import Champion, Match, Shape
+from core import Champion, Match, Shape, Team
+import socket, server, pickle
 
+
+# Return match summary
 def return_match_summary(match: Match) -> None:
 
     EMOJI = {
@@ -48,7 +51,7 @@ def return_match_summary(match: Match) -> None:
 
     return info
 
-
+# Return avalible champions function
 def return_available_champs(champions: dict[Champion]) -> None:
 
     # Create a table containing available champions
@@ -66,3 +69,39 @@ def return_available_champs(champions: dict[Champion]) -> None:
         available_champs.add_row(*champion.str_tuple)
 
     return available_champs
+
+# Validate champion pick function
+def validate_champion_pick(champion, player_number, champions, you, opponent):
+
+    if champion in you:
+        player_number.send(f'[{champion} is already in your team. Try again.]'.encode()) # Same method as in teamlocaltactics.py but modifed to send error message to the player directly
+    elif champion in opponent:
+        player_number.send(f'[{champion} is in the enemy team. Try again.]'.encode())
+    elif champion not in champions:
+        player_number.send(f'[The champion {champion} is not available. Try again.]'.encode())
+    else:
+        return True
+
+def get_scores(sel1: list, sel2: list, champions: dict[Champion], player1: socket, player2: socket):
+    
+    match = Match(
+        Team([champions[name] for name in sel1]),
+        Team([champions[name] for name in sel2])
+    )
+    match.play()
+
+    game_results = return_match_summary(match) # Using the function from teamnetworktactics to get match_summary of match
+
+    for i in range(len(game_results)):
+        server.delay(0.1)
+        player1.send("\n".encode())
+        player2.send("\n".encode())
+
+        server.delay(0.1)
+        player1.send("next message is pickled".encode())
+        player2.send("next message is pickled".encode())
+
+        server.delay(0.1)
+        pickle_champs = pickle.dumps(game_results[i])
+        player1.send(pickle_champs)
+        player2.send(pickle_champs)
